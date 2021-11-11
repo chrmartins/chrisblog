b91a910eecc07af99dc9395175842f5e6f544571
Christian Martins <cmts2010@gmail.com>
2021-11-11 14:48:00 -0300

    feat: add avatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../../src/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../src/styles/Home.module.css";
+
+import myAvatar from "../assets/meuavatar.png"
 
 export default function Home() {
   return (
@@ -12,23 +14,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Christian Martins
-        </h1>
+        <h1 className={styles.title}>Christian Martins</h1>
+        <h2>Desenvolvedor Front-end</h2>
+        <Image
+          src={myAvatar}
+          alt="Meu avatar"
+          width={200}
+          height={200}
+        />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
